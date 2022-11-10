ad5a495c2899f6ee2af79bda397a83046007c1e2
margotdv04 <mdraillardvandenberg@smith.edu>
2022-11-10 11:10:51 -0500

    added onto user interface

--- R/interface.R
+++ R/interface.R
@@ -1,15 +1,22 @@
 
 #' Beginning Interface
 #'
-#' @return data_cleaning()
+#' @return
 #' @export
 #'
 #' @examples
-start <- function(n = 1){
-  print("Hello! Welcome to SocialScienceR - an R package that teaches YOU how to use R for social science purposes.")
-  print("We will teach you basic data techniques and basic data visualization.")
-  n <- as.integer(readline(prompt = "Type 1 and press enter to continue"))
-  return(data_cleaning())
+start <- function(){
+  cat("Hello! Welcome to SocialScienceR - an R package that teaches YOU how to use R for social science purposes.\nWe will teach you basic data techniques and basic data visualization.\n")
+  n <- as.integer(readline(prompt = "Type 1 and press enter to continue "))
+  if(n == 1){
+    print("Success")
+  }
+  else{
+    print("Failure")
+  }
 }
+
+
+
 
 
