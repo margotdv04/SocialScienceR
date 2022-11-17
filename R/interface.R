d9da8e9b70116822dc3f4d7c8a8416a315861fb2
esaschenck <eschenck69@smith.edu>
2022-11-17 09:41:26 -0500

    changed start function name

--- R/interface.R
+++ R/interface.R
@@ -5,7 +5,7 @@
 #' @export
 #'
 #' @examples
-start <- function(){
+start_package <- function(){
   cat("Hello! Welcome to SocialScienceR - an R package that teaches YOU how to use R for social science purposes.\nWe will teach you basic data techniques and basic data visualization.\n")
   n <- as.integer(readline(prompt = "Type 1 and press enter to continue "))
   if(n == 1){
