cb665d1c0341a82720c4c3ba281b156de5051d3a
esaschenck <eschenck69@smith.edu>
2022-11-24 13:13:58 -0500

    ? in front of view_data function meant that the function didn't work... deleted the ? so now view_data works

--- R/view_data.R
+++ R/view_data.R
@@ -6,7 +6,7 @@
 #' @import dplyr
 #' @export
 #'
-?view_data <- function(){
+view_data <- function(){
   cat("Now you will learn to view data using the tidyverse package! \n")
   n <- readline(prompt = "Press enter to continue.")
   cat("First you need to view your data. You can simply write country_indicators; however, you can also view the data more conveniently, similarly to a spreadsheet. To do this, there's a command called view(). In the parentheses you will write the name of the dataset, which in this case is country_indicators. Type the following: \n")
