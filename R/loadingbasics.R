9379cb26882fa33cdcab4f431abdb81773c7cc16
margotdv04 <mdraillardvandenberg@smith.edu>
2022-12-06 10:52:05 -0500

    changed load_tidyverse() to load_dplyr()

--- R/loadingbasics.R
+++ R/loadingbasics.R
@@ -35,7 +35,7 @@ see_data_console <- function(){
 #' @export
 #'
 view_data <- function(){
-  cat("Now you will learn to view data using the tidyverse package! \n")
+  cat("Now you will learn to view data using the dplyr package! \n")
   n <- readline(prompt = "Press enter to continue.")
   cat("First you need to view your data. You can simply write country_indicators; however, you can also view the data more conveniently, similarly to a spreadsheet. To do this, there's a command called view(). In the parentheses you will write the name of the dataset, which in this case is country_indicators. Type the following: \n")
   cat("View(country_indicators)")
@@ -44,39 +44,38 @@ view_data <- function(){
   View(country_indicators)
 }
 
-
-#' Load tidyverse
+#' Load dplyr
 #'
 #' @description
-#' The function `load_tidyverse` guides the user through the installation of `tidyverse` (if neccessary) and loading the `tidyverse` package.
+#' The function `load_dplyr` guides the user through the installation of `dplyr` (if neccessary) and loading the `dplyr` package.
 #'
-#'@details
-#`load_tidyverse` asks the user if they've ever loaded the `tidyverse` package before, and if not, guides them through the installation.
-#' After `tidyverse` has been installed, or if it has been loaded before,
+#' @details
+#' `load_dplyr` asks the user if they've ever loaded the `dplyr` package before, and if not, guides them through the installation.
+#' After `dplyr` has been installed, or if it has been loaded before,
 #' the user is taught how to load the package using `library()`.
 #'
 #' @export
 #'
-#'
-load_tidyverse <- function(){
-  cat("The next step is to load a very important package called the 'tidyverse' \n")
-  cat("The tidyverse package is crucial to using R for data.")
+#' @examples
+load_dplyr <- function(){
+  cat("The next step is to load a very important package called 'dplyr' \n")
+  cat("The dplyr package is crucial to using R for data.")
   readline(prompt = "Press enter to continue.")
-  cat("If you've never used tidyverse before, you need to install it. ")
-  cat("Have you ever installed tidyverse? If so, type '1' and hit enter. If you've never installed tidyverse, type '2' and hit enter.")
+  cat("If you've never used dplyr before, you need to install it. ")
+  cat("Have you ever installed dplyr? If so, type '1' and hit enter. If you've never installed dplyr, type '2' and hit enter.")
   need_to_install <- as.integer(readline())
   if(need_to_install == 2){
-    cat("You will need to type 'install.packages(tidyverse)'. If you are successful, there will be red text that displays the contet type and how many KB of data were downloaded. Then type 1 and press enter when you have correctly loaded tidyverse")
+    cat("You will need to type 'install.packages(dplyr)'. If you are successful, there will be red text that displays the contet type and how many KB of data were downloaded. Then type 1 and press enter when you have correctly loaded tidyverse")
     next_step <- as.character(readline())
     if (next_step == 1){
-      cat("Even though we have tidyverse installed, we still need to load it.
-      Each time you restart R, you'll have to load tidyverse again.
+      cat("Even though we have dplyr installed, we still need to load it.
+      Each time you restart R, you'll have to load dplyr again.
       To load dplyr, type:
-      library(tidyverse)
+      library(dplyr)
       Now you try!")
-      typed_load_tidyverse <- as.character(readline())
-      check_if_correct(typed_load_tidyverse, "library(\"tidyverse\")")
-      library(tidyverse)
+      typed_load_dplyr <- as.character(readline())
+      check_if_correct(typed_load_dplyr, "library(\"dplyr\")")
+      library(dplyr)
       cat("Now you've successfully loaded the dplyr package.")
       see_data_console()
     }
@@ -86,4 +85,5 @@ load_tidyverse <- function(){
   }
 
 }
+
 
